5b5e73dc4050cc65856c3e76ec76ba569c2eda91
udochukwu <nnaji_udochukwu@yahoo.com>
2018-03-19 13:29:28 +0100

    update business routes

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -2,6 +2,9 @@ import express from 'express';
 
 import BusinessController from '../controllers/businessController';
 
+import ReviewController from '../controllers/reviewController';
+
+
 // import BusinessValidation from '../middlewares/BusinessValidation';
 
 
@@ -17,6 +20,8 @@ const {
   removeBusiness
 } = BusinessController;
 
+const { addReview, getAllReviews } = ReviewController;
+
 Router.get('/businesses', filterSearchByLocation, filterSearchByCategory, getAllBusinesses);
 
 Router.get('/businesses/:businessId', getBusinessById);
@@ -26,5 +31,9 @@ Router.post('/businesses', createBusiness);
 Router.post('/businesses/:businessId', updateBusiness);
 
 Router.delete('/businesses/:businessId', removeBusiness);
+
+Router.post('/businesses/:businessId/reviews', addReview);
+
+Router.get('/businesses/:businessId/reviews', getAllReviews);
 
 export default Router;
